4a547d409c210fb8ed333b6a488a77d5ea587d06
Langello <gastonchavo@gmail.com>
2021-11-12 22:44:56 -0300

    Update ActividadPracticaObligatoria.java

--- ActividadPracticaObligatoria/src/actividadpracticaobligatoria/ActividadPracticaObligatoria.java
+++ ActividadPracticaObligatoria/src/actividadpracticaobligatoria/ActividadPracticaObligatoria.java
@@ -15,7 +15,7 @@ public class ActividadPracticaObligatoria {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         
         Scanner sc=new Scanner(System.in);
         System.out.println("Ingrese nombre:");
