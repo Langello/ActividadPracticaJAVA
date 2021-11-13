2f6f0d6e472961eba3aea70fabebc8056e0d4c0e
Langello <gastonchavo@gmail.com>
2021-11-12 22:35:44 -0300

    Estapa1

---
+++ ActividadPracticaObligatoria/src/actividadpracticaobligatoria/ActividadPracticaObligatoria.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package actividadpracticaobligatoria;
+import java.util.Scanner;
+
+/**
+ *
+ * @author julian
+ */
+public class ActividadPracticaObligatoria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Ingrese nombre:");
+        String nombre = sc.nextLine();
+        System.out.println("Ingrese apellido:");
+        String apellido = sc.nextLine();
+        System.out.println("Ingrese edad:");
+        String edad = sc.nextLine();
+        System.out.println("Ingrese hobbie:");
+        String hobbie = sc.nextLine();
+        System.out.println("Ingrese editor de código favorito:");
+        String editorcodigo = sc.nextLine();
+        System.out.println("Ingrese sistema operativo que utiliza");
+        String sistemaoperativo = sc.nextLine();
+        
+        System.out.println("Nombre: "+nombre+" \nApellido: "+apellido+" \nEdad: "+edad+" \nHobbie: "+hobbie+" \nEditor de codigo favorito: "+editorcodigo+" \nSistema operativo que utiliza: "+sistemaoperativo);
+        
+        
+        
+        
+        
+    }
+    
+}
